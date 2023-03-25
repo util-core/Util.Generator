60852545b5df671ddb27bd43ef5922c246cf31b9
root <8907060@qq.com>
2023-03-25 11:34:09 +0800

    更新util版本

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -6,7 +6,7 @@
         /// <summary>
         /// Util框架版本号
         /// </summary>
-        public static string Util => "7.0.31";
+        public static string Util => "7.0.35";
         /// <summary>
         /// Microsoft.AspNetCore.TestHost版本号
         /// </summary>
