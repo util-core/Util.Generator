3b120fdc495aebeaeb5ac0e8f5f55de4de2519c4
root <8907060@qq.com>
2023-05-27 12:30:15 +0800

    添加清理脚本模板

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -1,67 +1,67 @@
 ﻿namespace Util.Generators.Helpers {
-    /// <summary>
-    /// 版本配置
-    /// </summary>
-    public static class Version {
-        /// <summary>
-        /// Util框架版本号
-        /// </summary>
-        public static string Util => "7.0.48";
-        /// <summary>
-        /// Microsoft.Extensions.Hosting版本号
-        /// </summary>
-        public static string Hosting => "7.0.1";
-        /// <summary>
-        /// Microsoft.AspNetCore.TestHost版本号
-        /// </summary>
-        public static string TestHost => "7.0.5";
-        /// <summary>
-        /// Microsoft.EntityFrameworkCore.Tools版本号
-        /// </summary>
-        public static string EntityFrameworkCoreTools => "7.0.5";
-        /// <summary>
-        /// Microsoft.EntityFrameworkCore.Design版本号
-        /// </summary>
-        public static string EntityFrameworkCoreDesign => "7.0.5";
-        /// <summary>
-        /// Microsoft.AspNetCore.Authentication.JwtBearer版本号
-        /// </summary>
-        public static string JwtBearer => "7.0.5";
-        /// <summary>
-        /// Microsoft.NET.Test.Sdk版本号
-        /// </summary>
-        public static string TestSdk => "17.5.0";
-        /// <summary>
-        /// xunit版本号
-        /// </summary>
-        public static string Xunit => "2.4.2";
-        /// <summary>
-        /// xunit.runner.visualstudio版本号
-        /// </summary>
-        public static string XunitRunner => "2.4.5";
-        /// <summary>
-        /// coverlet.collector版本号
-        /// </summary>
-        public static string Coverlet => "3.2.0";
-        /// <summary>
-        /// AutoBogus版本号
-        /// </summary>
-        public static string AutoBogus => "2.13.1";
-        /// <summary>
-        /// Moq版本号
-        /// </summary>
-        public static string Moq => "4.18.4";
-        /// <summary>
-        /// Xunit.DependencyInjection版本号
-        /// </summary>
-        public static string XunitDependencyInjection => "8.7.1";
-        /// <summary>
-        /// Xunit.DependencyInjection.Logging版本号
-        /// </summary>
-        public static string XunitLogging => "8.0.1";
-        /// <summary>
-        /// Swashbuckle.AspNetCore版本号
-        /// </summary>
-        public static string Swashbuckle => "6.5.0";
-    }
+	/// <summary>
+	/// 版本配置
+	/// </summary>
+	public static class Version {
+		/// <summary>
+		/// Util框架版本号
+		/// </summary>
+		public static string Util => "7.0.57";
+		/// <summary>
+		/// Microsoft.Extensions.Hosting版本号
+		/// </summary>
+		public static string Hosting => "7.0.1";
+		/// <summary>
+		/// Microsoft.AspNetCore.TestHost版本号
+		/// </summary>
+		public static string TestHost => "7.0.5";
+		/// <summary>
+		/// Microsoft.EntityFrameworkCore.Tools版本号
+		/// </summary>
+		public static string EntityFrameworkCoreTools => "7.0.5";
+		/// <summary>
+		/// Microsoft.EntityFrameworkCore.Design版本号
+		/// </summary>
+		public static string EntityFrameworkCoreDesign => "7.0.5";
+		/// <summary>
+		/// Microsoft.AspNetCore.Authentication.JwtBearer版本号
+		/// </summary>
+		public static string JwtBearer => "7.0.5";
+		/// <summary>
+		/// Microsoft.NET.Test.Sdk版本号
+		/// </summary>
+		public static string TestSdk => "17.6.0";
+		/// <summary>
+		/// xunit版本号
+		/// </summary>
+		public static string Xunit => "2.4.2";
+		/// <summary>
+		/// xunit.runner.visualstudio版本号
+		/// </summary>
+		public static string XunitRunner => "2.4.5";
+		/// <summary>
+		/// coverlet.collector版本号
+		/// </summary>
+		public static string Coverlet => "6.0.0";
+		/// <summary>
+		/// AutoBogus版本号
+		/// </summary>
+		public static string AutoBogus => "2.13.1";
+		/// <summary>
+		/// Moq版本号
+		/// </summary>
+		public static string Moq => "4.18.4";
+		/// <summary>
+		/// Xunit.DependencyInjection版本号
+		/// </summary>
+		public static string XunitDependencyInjection => "8.7.1";
+		/// <summary>
+		/// Xunit.DependencyInjection.Logging版本号
+		/// </summary>
+		public static string XunitLogging => "8.1.0";
+		/// <summary>
+		/// Swashbuckle.AspNetCore版本号
+		/// </summary>
+		public static string Swashbuckle => "6.5.0";
+	}
 }
