153600983f1543506211e03cb609604b9e8a02fb
root <8907060@qq.com>
2023-10-19 02:05:12 +0800

    更新生成配置

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -9,7 +9,7 @@ public static class Version {
     /// <summary>
     /// Util框架版本号
     /// </summary>
-    public static string Util => "7.1.42";
+    public static string Util => "7.1.59";
     /// <summary>
     /// Microsoft.Extensions.Hosting版本号
     /// </summary>
@@ -17,19 +17,19 @@ public static class Version {
     /// <summary>
     /// Microsoft.AspNetCore.TestHost版本号
     /// </summary>
-    public static string TestHost => "7.0.11";
+    public static string TestHost => "7.0.12";
     /// <summary>
     /// Microsoft.EntityFrameworkCore.Tools版本号
     /// </summary>
-    public static string EntityFrameworkCoreTools => "7.0.11";
+    public static string EntityFrameworkCoreTools => "7.0.12";
     /// <summary>
     /// Microsoft.EntityFrameworkCore.Design版本号
     /// </summary>
-    public static string EntityFrameworkCoreDesign => "7.0.11";
+    public static string EntityFrameworkCoreDesign => "7.0.12";
     /// <summary>
     /// Microsoft.AspNetCore.Authentication.JwtBearer版本号
     /// </summary>
-    public static string JwtBearer => "7.0.11";
+    public static string JwtBearer => "7.0.12";
     /// <summary>
     /// Microsoft.NET.Test.Sdk版本号
     /// </summary>
@@ -37,11 +37,11 @@ public static class Version {
     /// <summary>
     /// xunit版本号
     /// </summary>
-    public static string Xunit => "2.5.1";
+    public static string Xunit => "2.5.3";
     /// <summary>
     /// xunit.runner.visualstudio版本号
     /// </summary>
-    public static string XunitRunner => "2.5.1";
+    public static string XunitRunner => "2.5.3";
     /// <summary>
     /// coverlet.collector版本号
     /// </summary>
