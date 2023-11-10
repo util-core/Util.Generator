06bfa0ddfdf934ee0b8167ed6c40b8b46f9b5215
root <8907060@qq.com>
2023-11-10 09:49:16 +0800

    更新util版本

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -9,7 +9,7 @@ public static class Version {
     /// <summary>
     /// Util框架版本号
     /// </summary>
-    public static string Util => "7.1.68";
+    public static string Util => "7.1.73";
     /// <summary>
     /// Microsoft.Extensions.Hosting版本号
     /// </summary>
