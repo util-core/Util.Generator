e2fefba2c5a2de60d1d4a8f42eacd08240426f5a
root <8907060@qq.com>
2023-12-17 13:04:51 +0800

    表单操作弹出框改成抽屉方式

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -9,7 +9,7 @@ public static class Version {
     /// <summary>
     /// Util框架版本号
     /// </summary>
-    public static string Util => "7.1.92";
+    public static string Util => "7.1.111";
     /// <summary>
     /// Microsoft.Extensions.Hosting版本号
     /// </summary>
