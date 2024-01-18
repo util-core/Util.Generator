f0845e760d85f1e8dae18055c5b31a8a63167bea
utilcore <8907060@qq.com>
2024-01-18 19:10:35 +0800

    添加复制成功多语言键

--- src/Util.Generators.Templates.Helpers/Version.cs
+++ src/Util.Generators.Templates.Helpers/Version.cs
@@ -9,7 +9,7 @@ public static class Version {
     /// <summary>
     /// Util框架版本号
     /// </summary>
-    public static string Util => "7.1.129";
+    public static string Util => "7.1.131";
     /// <summary>
     /// Microsoft.Extensions.Hosting版本号
     /// </summary>
